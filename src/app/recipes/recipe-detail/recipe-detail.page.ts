53371f42bbd50866bf0bf8edd843179e6dde8c9a
eniskurtayyilmaz <kurtayyilmaz@gmail.com>
2020-02-27 09:27:10 +0300

    Angular component eklendi, Input ile componente veri inject edildi.

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../_service/recipes.service';
+import { AlertController } from '@ionic/angular';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,21 +16,45 @@ export class RecipeDetailPage implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private recipeService: RecipesService
+    private recipeService: RecipesService,
+    private router: Router,
+    private alertCtrl: AlertController
   ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
 
-      if (!paramMap.has("recipeId")) {
-        //redirect
+      if (!paramMap.has('recipeId')) {
+        this.router.navigate(['/recipes']);
         return;
       } else {
         const recipeId = paramMap.get('recipeId');
         this.recipe = this.recipeService.getRecipe(recipeId);
       }
     });
-    //this.recipe = this.recipeService.getRecipe
   }
 
+  deleteRecipe() {
+
+    const alert = this.alertCtrl.create({
+      header: 'Başlık!',
+      message: 'Bu kaydı silmek istiyor musunuz?',
+      buttons: [
+        {
+          text: 'İptal',
+          role: 'cancel',
+          cssClass: 'secondary',
+        }, {
+          text: 'Tamam',
+          handler: () => {
+            this.recipeService.onDeletedRecipe(this.recipe.id);
+            this.router.navigate(['/recipes']);
+          }
+        }
+      ]
+    });
+
+    alert.then(x => x.present());
+
+  }
 }
